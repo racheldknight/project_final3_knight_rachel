128fdaf753fc5a4bdf4223b938ff3e9776c8c9c7
Rachel Knight <dinkins.rachel@gmail.com>
2015-04-13 19:34:59 -0400

    Slick carousel plugin added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready ( function () {
     $('#Container').mixItUp();
+});
+
+$(document).ready ( function () {
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
 
 });
